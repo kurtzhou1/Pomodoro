c34a78dea9ee7b46a69916304c0dba5ccd47d006
Kurt <pandabat1@gmail.com>
2020-08-10 16:24:20 +0800

    增加倒數結束工作狀態切換成「已完成」狀態

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,8 +1,9 @@
 import React,{useState,useEffect,useRef} from 'react'
 import {useSelector,useDispatch} from 'react-redux'
 import {IState} from '../libs/common'
+import TodoList from './ToDoList'
 
-const Clock:React.FC= ()=>{
+const Clock:React.FC<{isDone?:boolean}>= (props) =>{
 
     // 工作時間
     const workTime = useSelector((state:IState) => state.time.workTime)
@@ -16,15 +17,16 @@ const Clock:React.FC= ()=>{
 
     // 顯示工作or休息時間Time
     const isWorkTime  = useRef<boolean>(true)
+    const isWorkTimeUp = useRef<boolean>(false) // Work Time結束，將工作未完成改成已完成
 
     // 暫停時間
     const storeRemainTime = useRef(workTime) // 傳出remainTime
     const timeIDRef =  useRef(0)// timeoutId 提供clear使用
+    const [isTimeStart,setIsTimeStart] = useState(false)
 
     const timeHandle = (time:number) => {
         
         const nowTime = Date.now()
-      
         return window.setInterval(()=>{
             let remainTime = 0
             const pastTime = (Date.now() - nowTime) / 1000
@@ -32,12 +34,12 @@ const Clock:React.FC= ()=>{
             const timeFormat = new Date(Math.round(remainTime)*1000).toISOString().substr(11,8)
             isWorkTime.current ? setRemainWorkSecond(timeFormat) : setremainBreakSecond(timeFormat)
                 storeRemainTime.current = remainTime
-            //檢查是否結束
         },1000)
       
     }
     const TimeStart = () => {
         timeIDRef.current = timeHandle(workTime)
+        setIsTimeStart(true)
     }
 
     const pauseTime = () => {
@@ -47,34 +49,40 @@ const Clock:React.FC= ()=>{
     const continueTime = () => {
         timeIDRef.current = timeHandle(storeRemainTime.current / 60)
     }
-
     
-
     useEffect(()=>{
         if(storeRemainTime.current <= 0 && isWorkTime.current){
-            pauseTime()
-            timeIDRef.current = timeHandle(breakTime)
-            isWorkTime.current = false
+            pauseTime() // 暫停
+            timeIDRef.current = timeHandle(breakTime) // 重啟休息時間
+            isWorkTime.current = false // 顯示為休息時間
+            isWorkTimeUp.current = true
         }else if (storeRemainTime.current <= 0 && !isWorkTime.current){
-            pauseTime()
-            isWorkTime.current = true
+            setIsTimeStart(false)
+            pauseTime() // 暫停
+            // isWorkTime.current = true
         }
     },[storeRemainTime.current])
 
+
     return(
         <>
+            <div>
+                {isWorkTimeUp.current ? '已完成':'未完成'}
+            </div>
             <div>
                 工作時間{remainWorkSecond}
             </div>
             <div>
                 休息時間{remainBreakSecond}
             </div>
-            <div onClick={()=> TimeStart()}>
-                開始
-            </div>
-            <div onClick={()=> pauseTime()}>
-                暫停
-            </div>
+            {isTimeStart ?
+                <div onClick={()=> pauseTime()}>
+                    暫停
+                </div> :
+                <div onClick={()=> TimeStart()}>
+                    開始
+                </div>
+            }
             <div onClick={()=> continueTime()}>
                 繼續
             </div>
