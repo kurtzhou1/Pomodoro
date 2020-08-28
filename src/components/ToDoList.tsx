387d4162dcd2ebb327272733176621b09c50cdf3
Kurt <pandabat1@gmail.com>
2020-08-28 13:54:09 +0800

    add remove list

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,7 +2,9 @@ import React,{useState} from 'react';
 import {IState} from '../libs/common';
 import { useSelector, useDispatch } from 'react-redux';
 import {CHANGE_TEXT,ADD_TODOLIST,DONE_TODOLIST,REMOVE_TODOLIST} from '../constant'
+import { PlusOutlined } from '@ant-design/icons';
 import './styles/comon.scss'
+import styles from './styles/css.module.scss';
 
 const ToDoList:React.FC = () => {
 
@@ -26,6 +28,13 @@ const ToDoList:React.FC = () => {
       }
   }
 
+  const removeEvent = (value:number) =>{
+    dispatch({
+      type:REMOVE_TODOLIST,
+      payload: {removeEvent:value}
+    })
+  }
+
   const finishEvent = ((value:number)=>{
     dispatch({
       type:DONE_TODOLIST,
@@ -35,18 +44,22 @@ const ToDoList:React.FC = () => {
 
   return (
     <div className="Pomodoro_module">
-      <input value={inputText} onChange={e=>changeText(e.target.value)} placeholder='type something'/>
-      <div onClick={()=>addEvent()}>+</div>
-      {todoList.map(data=>{
-        return(
-        <div>
-            <div>{data.id}</div>
-            <div>{data.toDoList}</div>
-            {/* <div onClick={()=>finishEvent(data.id)}>{data.isDone? '✔ 已完成':'✘ 未完成'}</div> */}
-        </div>
-        )
-      })}
-      
+      <input value={inputText} onChange={e=>changeText(e.target.value)} placeholder='type something and add'/>
+      <div className='add' onClick={()=>addEvent()}>＋</div>
+      <div className='item_wrap'>
+        {todoList.map(data=>{
+          return(
+          <div className='item'>
+              <div className='id'>{data.id}</div>
+              <div className='toDoList'>{data.toDoList}</div>
+              {/* <div onClick={()=>finishEvent(data.id)}>{data.isDone? '✔ 已完成':'✘ 未完成'}</div> */}
+              <div className={styles.todo_list_cancel} onClick={()=>removeEvent(data.id)}>
+                <PlusOutlined />
+              </div>
+          </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,4 @@
-import {CHANGE_TEXT,ADD_TODOLIST,DONE_TODOLIST,TIME_START} from '../constant'
+import {CHANGE_TEXT,ADD_TODOLIST,DONE_TODOLIST,REMOVE_TODOLIST} from '../constant'
 import {IState, todos} from '../libs/common'
 
 export const initState:IState = {
@@ -25,15 +25,22 @@ export const reducer = (state=initState , action:any):IState =>{
         case CHANGE_TEXT:
             return {...state,input:{text:action.payload.input}}
         case ADD_TODOLIST:
-            const tmp_add_toDoList = state.todos.splice(0)
+            const tmp_add_toDoList = state.todos.map(item=>item)
             const todoLength = tmp_add_toDoList.length
             tmp_add_toDoList.push({id:todoLength,toDoList:action.payload.addEvent,isDone: false})
             return {...state,todos:tmp_add_toDoList}
         case DONE_TODOLIST:
-            const tmp_done_toDoList = state.todos.splice(0)
+            const tmp_done_toDoList = state.todos.map(item=>item)
             tmp_done_toDoList[action.payload.finishEvent].isDone = !tmp_done_toDoList[action.payload.finishEvent].isDone
             console.log('id=>>',action.payload.finishEvent)
             return {...state,todos:tmp_done_toDoList}
+        case REMOVE_TODOLIST:
+            const tmp_remove_toDoList = state.todos.map(item=>item)
+            tmp_remove_toDoList.splice(action.payload.removeEvent,1)
+            for (let i=0; i<tmp_remove_toDoList.length;i++){
+                tmp_remove_toDoList[i].id = i
+            }
+            return {...state,todos:tmp_remove_toDoList}
         default:
             return state
     }
